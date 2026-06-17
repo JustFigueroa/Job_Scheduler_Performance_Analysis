a0815a49b3c07db08158403b869cadc41f03490e
Justin <Justinfigueroa99@icloud.com>
2026-06-17 14:32:44 -0700

    Project Specifications

--- Job_Scheduler_Performance_Analysis/main.cpp
+++ Job_Scheduler_Performance_Analysis/main.cpp
@@ -2,12 +2,37 @@
 //  main.cpp
 //  Job_Scheduler_Performance_Analysis
 //
+//  This program is designed to analyze the efficiency of different job scheduling algorithms.
+//
 //  Created by Justin Figueroa on 6/17/26.
 //
 
 #include <iostream>
 using namespace std;
 
+void RoundRobin2(vector<int>& jobTimes);
+void RoundRobin5(vector<int>& jobTimes);
+void FirstComeFirstServe(vector<int>& jobTimes);
+void ShortestJobFirst(vector<int>& jobTimes);
+void randomJobGenerator();
+
 int main(int argc, const char * argv[]) {
     
 }
+
+void RoundRobin2(vector<int>& jobTimes){
+    
+}
+
+void RoundRobin5(vector<int>& jobTimes){
+    
+}
+
+void FirstComeFirstServe(vector<int>& jobTimes){
+    
+}
+
+void ShortestJobFirst(vector<int>& jobTimes){
+    
+}
+randomJobGenerator();
