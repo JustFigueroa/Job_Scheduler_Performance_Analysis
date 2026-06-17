3038d6efff6ae951327f9337e858347c5fcc2cbb
Justin <Justinfigueroa99@icloud.com>
2026-06-17 14:48:08 -0700

    Add initial Job Scheduler Program Structure

--- Job_Scheduler_Performance_Analysis/main.cpp
+++ Job_Scheduler_Performance_Analysis/main.cpp
@@ -8,16 +8,38 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <fstream>
 using namespace std;
 
-void RoundRobin2(vector<int>& jobTimes);
+//randomJobGenrator generates a job.txt file with random job info as required by project specifications.
+void randomJobGenerator();
+//getJobFile will load the job.txt file to be parsed by jobTimes function.
+ifstream getJobFile();
+//parseJobFile will parse input file jobList to return a vector with job times.
+vector<int> parseJobFile(ifstream& jobList);
+//The four algorthms to be analyzed: RR (Times Slices 2 and 5 minutes), FCFS, SJF.
+void RoundRobin2(vector<int>& jobTime);
 void RoundRobin5(vector<int>& jobTimes);
 void FirstComeFirstServe(vector<int>& jobTimes);
 void ShortestJobFirst(vector<int>& jobTimes);
-void randomJobGenerator();
+
+
 
 int main(int argc, const char * argv[]) {
+    randomJobGenerator();
+    ifstream jobList = getJobFile();
     
+}
+
+void randomJobGenerator(){
+    
+}
+
+vector<int> parseJobFile(ifstream& jobList){
+    vector<int> jobTimes;
+    
+    return jobTimes;
 }
 
 void RoundRobin2(vector<int>& jobTimes){
@@ -35,4 +57,4 @@ void FirstComeFirstServe(vector<int>& jobTimes){
 void ShortestJobFirst(vector<int>& jobTimes){
     
 }
-randomJobGenerator();
+
